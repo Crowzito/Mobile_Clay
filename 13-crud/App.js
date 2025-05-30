4c1682183748dbf2bf7474eec525d3fb95d7114e
unknown <23214290046@academico.iesb.br>
2025-05-30 19:38:08 -0300

    correcao 13 crud

--- 13-crud/App.js
+++ 13-crud/App.js
@@ -1,26 +1,16 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-
-import { PaperProvider } from "react-native-paper";
-import StackRoutes from "./routers/StackRoutes";
-
 import { NavigationContainer } from "@react-navigation/native";
+import { PaperProvider } from "react-native-paper";
+import AlunoStack from "./src/alunos/AlunoStack";
 
 export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <StackRoutes />
+        <AlunoStack />
       </NavigationContainer>
     </PaperProvider>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
--- README.md
+++
@@ -1,4 +0,0 @@
-# IESB - Programação de Dispositivos Mobile
-
-# Pedro Victor Souza - 23214290046  
-
